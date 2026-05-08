94735c9ec43d96b8ea0c221674a86566c75e8245
Leo <146775205+LeoAboard@users.noreply.github.com>
2026-05-08 06:26:25 -0400

    fix: operação errada
    Updated complexity analysis comments to reflect changes in loop conditions.

--- CountingSort/code/Counting_Sort_analise.c
+++ CountingSort/code/Counting_Sort_analise.c
@@ -6,7 +6,7 @@ void ordenacao_contagem(int vetor[], int tamanho)
     int valor_maximo = 0;                   //c1
 
     for (int i = 0; i < tamanho; i++)       //c2: inicialização
-                                            //c3: teste = n-1 => c3*(n-1)
+                                            //c3: teste = n+1 => c3*(n+1)
                                             //c4: passo = n
     {                                       
         if (vetor[i] > valor_maximo)        //c5: n*c5
@@ -18,14 +18,14 @@ void ordenacao_contagem(int vetor[], int tamanho)
     int* vetor_contagem = (int*)calloc(valor_maximo + 1, sizeof(int));  //c7: c7*(k+1)
 
     for (int i = 0; i < tamanho; i++)       //c8: inicialização
-                                            //c9: teste = n-1 => c9*(n-1)
+                                            //c9: teste = n+1 => c9*(n+1)
                                             //c10: passo = n
     {
         vetor_contagem[vetor[i]]++;         //c11: n
     }
 
     for (int i = 1; i <= valor_maximo; i++) //c12: inicialização
-                                            //c13: teste = k-1 => c13(k-1)
+                                            //c13: teste = k+1 => c13(k+1)
                                             //c14: passo = k
     {
         vetor_contagem[i] += vetor_contagem[i - 1];         //c15: k
@@ -34,7 +34,7 @@ void ordenacao_contagem(int vetor[], int tamanho)
     int* resultado = (int*)malloc(tamanho * sizeof(int));   //c16: n
 
     for (int i = tamanho - 1; i >= 0; i--)                  //c17: inicialização
-                                                            //c18: teste = n-1 => 
+                                                            //c18: teste = n+1 => c18*(n+1)
                                                             //c19: passo = n
     {
         resultado[vetor_contagem[vetor[i]] - 1] = vetor[i]; //c20: n
@@ -42,7 +42,7 @@ void ordenacao_contagem(int vetor[], int tamanho)
     }
 
     for (int i = 0; i < tamanho; i++)       //c22: inicialização 
-                                            //c23: teste = n-1
+                                            //c23: teste = n+1 => c23*(n+1)
                                             //c24: passo = n
     {
         vetor[i] = resultado[i];            //c25: n
@@ -54,9 +54,9 @@ void ordenacao_contagem(int vetor[], int tamanho)
 
 /* ========= ANÁLISE DE COMPLEXIDADE ==========
 
-    T(n,k) = c1 + (c2 + c3(n−1) + c4 n + c5 n + c6 n) + c7(k+1) + (c8 + c9(n−1) + c10 n + c11 n) +
-            (c12 + c13(k−1) + c14k + c15k) + c16 n + (c17 + c18(n−1) + c19 n + c20 n + c21 n) + 
-            (c22 + c23(n−1) + c24 n +c25 n) + c26 + c27
+    T(n,k) = c1 + (c2 + c3(n+1) + c4 n + c5 n + c6 n) + c7(k+1) + (c8 + c9(n+1) + c10 n + c11 n) +
+            (c12 + c13(k+1) + c14k + c15k) + c16 n + (c17 + c18(n+1) + c19 n + c20 n + c21 n) + 
+            (c22 + c23(n+1) + c24 n +c25 n) + c26 + c27
 
     O(n + k)
 */
